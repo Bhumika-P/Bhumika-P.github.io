fcbdaba94f8ba40fd58794f5e017edd9a14099d7
Bhumika-P <bhumika.9117@gmail.com>
2022-08-02 20:44:25 -0500

    Modified skills

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,31 +1,3 @@
-var v1 = document.getElementById("h_about");
-var v2 = document.getElementById("h_skills");
-var v3 = document.getElementById("h_career");
-var v4 = document.getElementById("h_projects");
-var v5 = document.getElementById("h_contact");
-var v6 = document.getElementById("name");
-var v7 = document.getElementById("resume");
-function to_about() {
-  v1.scrollIntoView(true);
-}
-function to_skills() {
-  v2.scrollIntoView(true);
-}
-function to_career() {
-  v3.scrollIntoView(true);
-}
-function to_projects() {
-  v4.scrollIntoView(true);
-}
-function to_contact() {
-  v5.scrollIntoView(true);
-}
-function to_home() {
-  v6.scrollIntoView(true);
-}
-function to_resume() {
-  v7.scrollIntoView(true);
-}
 function sendmail() {
   window.open('mailto:test@example.com?subject=subject&body=body');
 }
@@ -65,8 +37,8 @@ var lang = {
   "html": "90%",
   "css": "80%",
   "javascript": "70%",
-  "java": "78%",
-  "sql": "40%",
+  "java": "80%",
+  "sql": "90%",
   "as": "90%"
 };
 
